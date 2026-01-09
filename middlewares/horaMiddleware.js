438d5fea4374e94e13af4d8edc4d644ea2811a8f
Sara Isabel del Sastre <chinchimilla@gmail.com>
2026-01-09 22:21:33 +0100

    Timepass terminado

---
+++ middlewares/horaMiddleware.js
@@ -0,0 +1,9 @@
+
+module.exports = (req, res, next) =>{
+const now = new Date();
+const hora = now.getHours();
+const minutos = now.getMinutes().toString().padStart(2, "0")
+req.horaActual = now.getHours()
+req.horaTotal =`${req.horaActual}:${minutos}`
+next()
+};
